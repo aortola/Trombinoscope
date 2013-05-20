c2b1fadfd8aad1e4e0271ed5f18527cc9af992a3
aortola <adrien.ortola@imerir.com>
2013-05-20 19:21:13 +0200

    [IHM] - Les balbutiements de l'IHM
    Ajout des Layouts basiques qui serviront aux tests fonctionnels durant
    le développement de l'appli. Ils fournissent aussi un support graphique
    simpliste.

--- Trombinoscope/gen/imerir/android/trombinoscope/R.java
+++ Trombinoscope/gen/imerir/android/trombinoscope/R.java
@@ -13,20 +13,48 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int photo_profil_vide=0x7f020002;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int button1=0x7f070001;
+        public static final int button2=0x7f070008;
+        public static final int checkBox1=0x7f07000d;
+        public static final int editText1=0x7f070003;
+        public static final int editText2=0x7f070005;
+        public static final int editText3=0x7f070007;
+        public static final int imageView1=0x7f070000;
+        public static final int img=0x7f07000a;
+        public static final int listeprofils=0x7f070009;
+        public static final int menu_settings=0x7f07000e;
+        public static final int nom=0x7f07000b;
+        public static final int prenom=0x7f07000c;
+        public static final int textView1=0x7f070002;
+        public static final int textView2=0x7f070004;
+        public static final int textView3=0x7f070006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int creation_profil_formulaire=0x7f030001;
+        public static final int liste_profils=0x7f030002;
+        public static final int profil_element_liste=0x7f030003;
+        public static final int rechercher_profil_formulaire=0x7f030004;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
+        public static final int ajouter=0x7f04000c;
+        public static final int ajouterprofil=0x7f04000b;
         public static final int app_name=0x7f040000;
+        public static final int avecphoto=0x7f04000a;
+        public static final int groupe=0x7f040008;
         public static final int hello_world=0x7f040001;
+        public static final int hint_vide=0x7f040007;
         public static final int menu_settings=0x7f040002;
+        public static final int nom=0x7f040005;
+        public static final int prendrephoto=0x7f040004;
+        public static final int prenom=0x7f040006;
+        public static final int rechercher=0x7f040009;
         public static final int title_activity_main=0x7f040003;
     }
     public static final class style {
